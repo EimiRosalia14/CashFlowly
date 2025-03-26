acf63a2e4e6a38c6e2ed2c4b5256fa4442879a51
Ravelo-Dev <zmarcos140@gmail.com>
2025-03-25 21:13:10 -0400

    Servicio para recomendaciones en metas financieras

---
+++ CashFlowly.Core.Application/Services/MetasRecommendationService.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CashFlowly.Core.Application.Interfaces.Services;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
+
+namespace CashFlowly.Core.Application.Services
+{
+    public class MetasRecommendationService : IMetasRecommendationService
+    {
+        private readonly IMetaService _metaService;
+        private readonly IConfiguration _configuration;
+        private readonly ILogger<MetasRecommendationService> _logger;
+        private readonly IHttpClientFactory _httpClientFactory;
+
+        public MetasRecommendationService(IMetaService metaService, IConfiguration configuration, ILogger<MetasRecommendationService> logger, IHttpClientFactory httpClientFactory)
+        {
+            _metaService = metaService;
+            _configuration = configuration;
+            _logger = logger;
+            _httpClientFactory = httpClientFactory;
+        }
+
+        public Task<string> GetGoalsRecommendationsAsync(int usuarioId)
+        {
+            
+        }
+    }
+}
